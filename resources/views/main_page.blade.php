cd0b4b243ad6bd89c2cb431300f5d5d5d92bb72f
jpspeak <jayphilipcamillo@gmail.com>
2020-09-19 21:41:33 +0800

    Added Roboto Font cdn

--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -5,9 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pinoy Recipes</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
     <!-- Styles -->
     <link href="{{asset('css/app.css')}}">
-
     <!-- Scripts -->
     <script defer src="{{asset('js/app.js')}}"></script>
 </head>
